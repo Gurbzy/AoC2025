798c096bdeb3348f95d8413db0b995d3a485d72c
Gurbzy <mikey.gur@gmail.com>
2025-12-03 15:06:20 -0800

    no build

--- build/CMakeFiles/aoc.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/aoc.dir/src/main.cpp.o: \
- /mnt/c/Users/michael/Desktop/coderepos/AoC2025/src/main.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/michael/Desktop/coderepos/AoC2025/src/days/day1.h \
- /mnt/c/Users/michael/Desktop/coderepos/AoC2025/src/days/day2.h
